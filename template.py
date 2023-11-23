44701b5e719c0d7b7a4f7099da5fd365bea73c57
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2023-11-23 22:37:12 +0200

    file structure added

---
+++ template.py
@@ -0,0 +1,46 @@
+"""
+Auto project file structure creator.
+"""
+import os 
+from pathlib import Path
+import logging
+
+# logging
+logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s]: %(message)s')
+
+PROJECT_NAME = "LeukemiaAI"
+
+FILES_PATHS = [
+    ".github/workflows/.gitkeep",
+    f"src/{PROJECT_NAME}/__init__.py",
+    f"src/{PROJECT_NAME}/components/__init__.py",
+    f"src/{PROJECT_NAME}/utils/__init__.py",
+    f"src/{PROJECT_NAME}/config/__init__.py",
+    f"src/{PROJECT_NAME}/config/configuration.py",
+    f"src/{PROJECT_NAME}/pipeline/__init__.py",
+    f"src/{PROJECT_NAME}/entity/__init__.py",
+    f"src/{PROJECT_NAME}/constants/__init__.py",
+    "config/config.yml",
+    "dvc.yml",
+    "params.yml",
+    "requirements.txt",
+    "requirements.dev.txt",
+    "setup.py",
+    "research/trials.ipynb"
+]
+
+# creating files.
+for f_path in FILES_PATHS:
+    f_dir, f_name = os.path.split(Path(f_path))
+
+    if f_dir != "":
+        os.makedirs(f_dir, exist_ok=True)
+        logging.info(f"created dir: [{f_dir}]")
+
+    if not os.path.exists(f_path):
+        with open(f_path, 'w') as f:
+            logging.info(f"created file [{f_name}]")
+        
+    else:
+        logging.info(f"[{f_name}], already created !")
+
